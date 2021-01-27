6459f01cafd5433e1378f05da0760b793ed65d4f
FanBOT2 <78063700+FanBOT2@users.noreply.github.com>
2021-01-27 18:32:31 +0700

    Add files via upload

---
+++ donate.js
@@ -0,0 +1,34 @@
+exports.donate = (id, FUNNY BOT, corohelp, tampilTanggal, tampilWaktu, instagram, nomer, aktif, groupwhatsapp, youtube) => {
+	return `
+  
+❉──────────❉
+  Hi. *${id.split("@s.whatsapp.net")[0]}* 👋️
+❉──────────❉
+
+╔════════════════════
+║ *Donasi Ke ${FUNNY BOT}*
+╠════════════════════
+║├≽️⚜ *PULSA* : _0838-7544-5000_
+║├≽️⚜ *DANA* : _0895414834921_
+╠════════════════════
+║  *${FUNNY BOT}*
+╠════════════════════
+║╭──❉ *SOSMED ADMIN* ❉──
+║│1. *Group WhatsApp*
+║│ _${https://chat.whatsapp.com/Hxf0XTETWLu4quYbFWY2SS}_
+║│2. *YouTube <subscribe>*
+║│ _${IRFANNN YT GAMMING}_
+║│3. *Instagram <Follow>*
+║│ _${IRFANNNSTORE}_
+║│4. *Creator ${XBOT}*
+║│ _${083875445000}_
+║╰───────────
+╠════════════════════
+║ _*MADE BY IRFAN SATRIAWAN*_
+╠════════════════════
+║ゞ📆 *${tampilTanggal}*
+║ゞ⏱️ *${tampilWaktu}*
+║ゞ📢 Bot Aktif ; *${aktif}*
+╚════════════════════`
+}
+
---
+++ info.js
@@ -0,0 +1,34 @@
+exports.info = (id, FUNNY BOT, corohelp, tampilTanggal, tampilWaktu, instagram, nomer, aktif, groupwa, youtube) => {
+	return `*MENU ${XBOT}*
+  
+❉──────────❉
+  Hi. *${id.split("@s.whatsapp.net")[0]}* 👋️
+❉──────────❉
+
+╔════════════════════
+║ *Info ${FUNNY BOT}*
+╠════════════════════
+║├≽️⚜ *AUTOR* : _Irfannn Satriawan_
+║├≽️⚜ *DESIGNER* : _AMPIBI_
+║├≽️⚜ *OWNER* : _IRFAN SATRIAWAN_
+╠════════════════════
+║  *${FUNNY BOT}*
+╠════════════════════
+║╭──❉ *SOSMED ADMIN* ❉──
+║│1. *Group WhatsApp*
+║│ _${https://chat.whatsapp.com/Hxf0XTETWLu4quYbFWY2SS}_
+║│2. *YouTube <subscribe>*
+║│ _${IRFANNN YT GAMMING}_
+║│3. *Instagram <Follow>*
+║│ _${Irfannnstore}_
+║│4. *Creator ${FUNNYBOT}*
+║│ _${083875445000}_
+║╰───────────
+╠════════════════════
+║ _*MADE BY Irfan Satriawan*_
+╠════════════════════
+║ゞ📆 *${tampilTanggal}*
+║ゞ⏱️ *${tampilWaktu}*
+║ゞ📢 Bot Aktif ; *${aktif}*
+╚════════════════════`
+}
